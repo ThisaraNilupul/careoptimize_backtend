666c0ae3f8d845d8acaa2db064ea3c60a087ba25
ThisaraNilupul <thisaranilupul99@gmail.com>
2024-09-25 23:38:56 +0530

    Patient profile page routes, controllers and models added

--- controllers/patientControllers.js
+++ controllers/patientControllers.js
@@ -2,17 +2,99 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Patient = require('../models/patientModel');
 
+
 //get patient profile
 exports.getPatientProfile = async (req, res) => {
     try{
         // Find the patient by the ID attached by the auth middleware
-        const patient = await Patient.findById(req.user).select('-password');
+        const patient = await Patient.findById(req.params.id);
         if (!patient) {
-            res.status(404).json({ msg: 'Patient not found'});
+            return res.status(404).json({ msg: 'Patient not found'});
         }
         res.json(patient);
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Server error')
+    }
+};
+
+//update medical bio-data
+exports.addMedicalBioData = async (req, res) => {
+    const { bloodType, height, weight, healthIssues } = req.body;
+
+    try {
+        const patient = await Patient.findById(req.params.id);
+        if(!patient){
+            return res.status(404).json({msg: 'Patient not found'});
+        }
+        patient.bloodType = bloodType || patient.bloodType;
+        patient.height = height || patient.height;
+        patient.weight = weight || patient.weight;
+        if (Array.isArray(healthIssues) && healthIssues.length){
+            patient.healthIssues.push(...healthIssues.map(issue => ({issue})));
+        }
+
+        await patient.save();
+        res.status(200).json({msg: 'Medical bio-data updated successfully', patient});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({error: error.message});
+    }
+};
+
+//get medical bio-data
+exports.getMedicalBio = async (req, res) => {
+    try{
+        const patient = await Patient.findById(req.params.id);
+        if (!patient){
+            return res.status(404).json({msg: 'Patient not found'});
+        }
+
+        const medicalBioData = {
+            bloodType: patient.bloodType,
+            height: patient.height,
+            weight: patient.weight,
+            healthIssues: patient.healthIssues
+        }
+
+        res.status(200).json(medicalBioData);
+    }catch (error) { 
+        console.error(error.message);
+        res.status(500).json({ error: error.message });
+    }
+};
+
+//add patient's relatives
+exports.addrelative = async (req, res) => {
+    const { firstName, lastName, addressNo, street, city, province, nic, phoneNumber, email, relationship} = req.body;
+
+    try{
+        const patient = await Patient.findById(req.params.id);
+        if (!patient){
+            return res.status(404).json({msg: 'Patient not found'});
+        }
+
+        const newRelative = { firstName, lastName, addressNo, street, city, province, nic, phoneNumber, email, relationship};
+        patient.relatives.push(newRelative);
+
+        await patient.save();
+        res.status(200).json({msg: 'Relative added successfully', patient});
+    }catch (error){
+        console.error(error.message);
+        res.status(500).json({ error: error.message });
+    }
+};
+
+//get all patient's relatives
+exports.getRelatives = async (req, res) => {
+    try{
+        const patient = await Patient.findById(req.params.id);
+        if (!patient) {
+            return res.status(404).json({ msg: 'Patient not found' });
+        }
+        res.status(200).json(patient.relatives);
+    }catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: error.message });
     }
 };
--- models/patientModel.js
+++ models/patientModel.js
@@ -1,5 +1,24 @@
 const mongoose = require('mongoose');
 
+//sub-schema for health issues
+const healthIssueSchema = new mongoose.Schema({
+    issue: {type: String}
+});
+
+//sub-schema for relatives
+const relativesSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true},
+    addressNo: { type: String, required: true},
+    street: { type: String, required: true},
+    city: { type: String, required: true},
+    province: { type: String, required: true},
+    nic: {type: String, required: true, unique: true},
+    phoneNumber: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    relationship: { type: String, required: true }
+});
+
 const patientSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true},
@@ -13,8 +32,13 @@ const patientSchema = new mongoose.Schema({
     birthday: {type: Date, required: true},
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    
+    bloodType: {type: String, required: true},
+    height: { type: Number, required: true },
+    weight: { type: Number, required: true },
+    healthIssues: [healthIssueSchema],
+    relatives: [relativesSchema]
 }, {timestamps: true});
 
 const patient = mongoose.model('patient', patientSchema);
 module.exports = patient;
-
