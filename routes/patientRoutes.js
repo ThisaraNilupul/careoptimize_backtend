37bda645f492c6a8525ad4979ccebe27230ff167
ThisaraNilupul <thisaranilupul99@gmail.com>
2024-09-29 18:19:45 +0530

    patient profile page new controllers and routes added

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -2,7 +2,22 @@ const express = require('express');
 const { check } = require('express-validator');
 const {registerPatient} = require('../controllers/patientRegisterController');
 const {loginPatient} = require('../controllers/patientLoginController');
-const {getPatientProfile, addMedicalBioData, getMedicalBio, addrelative, getRelatives} = require('../controllers/patientControllers');
+const {
+        getPatientProfile, 
+        addMedicalBioData, 
+        getMedicalBio, 
+        addrelative, 
+        getOneRelative, 
+        getRelatives, 
+        deleteOneRelative, 
+        updateOneRelative, 
+        editPatientProfile,
+        addHealthIssue,
+        getAllHealthIssues,
+        deleteHealthIssue,
+        updateHealthIssue,
+        getOneHealthIssue
+      } = require('../controllers/patientControllers');
 const auth = require('../middleware/auth');
 
 const router = express.Router();
@@ -44,6 +59,9 @@ router.post('/login',
 //Get patient's profile
 router.get('/profile/:id', getPatientProfile);
 
+//edit patient's profile data
+router.put('/Profile/:id', editPatientProfile)
+
 //Add patient's Medical-Bio data
 router.put('/profile/:id/medical-bio', addMedicalBioData);
 
@@ -53,8 +71,32 @@ router.get('/profile/:id/medical-bio', getMedicalBio);
 //Add patient's relatives
 router.post('/profile/:id/relative', addrelative);
 
+//get a one patient's relative
+router.get('/profile/:id/:rid', getOneRelative)
+
 //get all patient's relatives
 router.get('/profile/:id/relatives', getRelatives);
+
+//delete a patient's relative
+router.delete('/profile/:id/:rid', deleteOneRelative);
+
+//update a patient's relative
+router.put('/profile/:id/:rid', updateOneRelative);
+
+//add patient health issues
+router.post('/profile/:id/health-issues', addHealthIssue);
+
+//get all patient's health issues
+router.get('profile/:id/health-issues', getAllHealthIssues);
+
+//get a one patient's health issue
+router.get('/profile/:id/health-issues/:issueId', getOneHealthIssue);
+
+//delete a health issue
+router.delete('/profile/:id/health-issues/:issueId', deleteHealthIssue);
+
+//update a health issue
+router.put('/profile/:id/health-issues/:issueId', updateHealthIssue);
 
 
 module.exports = router;
