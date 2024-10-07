780835195ee46f079faebc34967def07c35d2e39
ThisaraNilupul <thisaranilupul99@gmail.com>
2024-10-08 00:36:44 +0530

    doctor profile routes and controllers added

---
+++ routes/doctorRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { check } = require('express-validator');
+const { getDoctorProfile,
+        editDoctorProfile,
+        addDoctorInfo,
+        getDoctorInfo,
+        addDoctorWorkAt,
+        editDoctorWorkAt,
+        getDoctorWorkAt,
+        deleteDoctorWorkAt
+ } = require('../controllers/doctorControllers');
+ const auth = require('../middleware/auth');
+
+const router = express.Router();
+
+//Get doctors's profile
+router.get('/profile/:id', getDoctorProfile);
+
+//edit doctor's profile
+router.put('/profile/:id', editDoctorProfile);
+
+//add doctor's doctorInfo
+router.put('/profile/:id/doctor-info', addDoctorInfo);
+
+//get doctor's doctorInfo
+router.get('/profile/:id/doctor-info', getDoctorInfo);
+
+//add doctor's work at info
+router.post('/profile/:id/work-at', addDoctorWorkAt);
+
+//edit doctor's work at info
+router.put('/profile/:id/work-at/:wid', editDoctorWorkAt);
+
+//get one doctor's work at info
+router.get('/profile/:id/work-at/:wid', getDoctorWorkAt);
+
+//delete one doctor's work at info
+router.delete('/profile/:id/work-at/:wid', deleteDoctorWorkAt);
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const patientRoutes = require('./routes/patientRoutes');
+const doctorRoutes = require('./routes/doctorRoutes');
 const registerRoute = require('./routes/registerRoute');
 
 //load env veriables
@@ -20,6 +21,7 @@ app.use(cors());
 //routes
 app.use('/api/', registerRoute);
 app.use('/api/patient', patientRoutes);
+app.use('/api/doctor', doctorRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
